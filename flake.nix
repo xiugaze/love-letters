27a05582e079f80751d10c7b91650b977dbfe5da
Caleb Andreano <calebandreano@gmail.com>
2025-01-01 18:00:58 -0600

    integrate for flake config

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "love letters";
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, gomod2nix, utils}: 
+  utils.lib.eachSystem [
+    "x86_64-linux"
+  ] (system: 
+    let 
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ gomod2nix.overlays.default ];
+      };
+    in {
+      packages = {
+        default = pkgs.buildGoApplication {
+          pname = "love-letters";
+          version = "0.0.1";
+          src = ./.;
+          modules = ./gomod2nix.toml;
+          buildinputs = with pkgs; [ ];
+
+          postInstall = ''
+              mkdir -p $out/share/love-letters
+              cp -r ./static $out/share/love-letters/
+          '';
+        };
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+          gotools
+          go-tools
+          gomod2nix.packages.${system}.default
+        ];
+      };
+  });
+}
+
--- main.go
+++ main.go
@@ -1,52 +1,83 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
+  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "os"
+  "time"
 )
 
+const DEFAULT_PORT string = "8080"
+var static string
+var media string
+var port string
+
+func get_vars() (string, string, string) {
+  static := os.Getenv("STATIC_FILES_PATH")
+  if static == "" {
+    static = "./static"
+  }
+  media := os.Getenv("MEDIA_FILES_PATH")
+  if media == "" {
+    media = "./media"
+  }
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = DEFAULT_PORT
+  }
+  return static, media, port
+}
+
+/* handle post request containing svg */
+func save_handler(w http.ResponseWriter, r *http.Request) {
+  // go uses 2006-01-02 15:04:05 for time format referencing
+  time := time.Now().Format("2006-01-02")
+  if r.Method != http.MethodPost {
+    http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    return
+  }
+
+  body, err := io.ReadAll(r.Body)
+  if err != nil {
+    http.Error(w, "Failed to read request body (invalid .svg?)", http.StatusInternalServerError)
+  }
+  err = os.WriteFile(media + "/" + time + ".svg", body, 0644) // rw-r--r--
+
+  if err != nil {
+    http.Error(w, "Failed to save SVG", http.StatusInternalServerError)
+    return
+  }
+  fmt.Fprintln(w, "saved successfully")
+}
+
+func get_handler(w http.ResponseWriter, r *http.Request) {
+  time := time.Now().Format("2006-01-02")
+  if r.Method != http.MethodGet {
+    http.Error(w, "Invalid Request method", http.StatusMethodNotAllowed)
+    return
+  }
+  svg, err := os.ReadFile(media + "/" + time + ".svg")
+  if err != nil {
+    http.Error(w, "Failed to read SVG file (does file exist?)", http.StatusInternalServerError)
+    return
+  }
+  w.Header().Set("Content-Type", "image/svg+xml")
+  w.Write(svg)
+}
+
 func main() {
-    fs := http.FileServer(http.Dir("./static"))
-    http.Handle("/", fs)
-    http.HandleFunc("/save-message", func(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-	    http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	    return
-	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-	    http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-	    return
-	}
-	err = os.WriteFile("msg.svg", body, 0644)
-	if err != nil {
-	    http.Error(w, "Failed to save SVG", http.StatusInternalServerError)
-	    return
-	}
-	fmt.Fprintln(w, "SVG saved successfully")
-    })
+  static, media, port = get_vars()
+  http.Handle("/", http.FileServer(http.Dir(static)))
 
+  http.HandleFunc("/save", save_handler)
+  http.HandleFunc("/get", get_handler)
 
-    http.HandleFunc("/get-svg", func(w http.ResponseWriter, r *http.Request) { 
-	if r.Method != http.MethodGet {
-	    http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	    return
-	}
-	svg, err := os.ReadFile("msg.svg")
-	if err != nil {
-	    http.Error(w, "Failed to read SVG file", http.StatusInternalServerError)
-	    return
-	}
-	w.Header().Set("Content-Type", "image/svg+xml")
-	w.Write(svg)
-    })
-
-    log.Println("Serving on port 9999")
-    err := http.ListenAndServe(":9999", nil)
-    if err != nil {
-	panic(err)
-    }
+  log.Println("Serving on port " + port)
+  err := http.ListenAndServe(":" + port, nil)
+  if err != nil {
+    panic(err)
+  }
 }
